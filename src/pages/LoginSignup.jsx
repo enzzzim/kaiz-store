ab6305870fea0bc94c9f10e98d2246c866111d09
Enzo <enzimskx@gmail.com>
2025-11-05 17:00:22 -0300

    feat: Adiciona página para criação de cadastro e login.

--- src/pages/LoginSignup.jsx
+++ src/pages/LoginSignup.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import './CSS/LoginSignup.css'
 
 export const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
+    <div className='loginsignup'>
+      <div className="loginsignup-container">
+        <h1>Cadastre-se</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder='Seu Nome' />
+          <input type="text" placeholder='Endereço de e-mail' />
+          <input type="text" placeholder='Senha' />
+        </div>
+        <button>Continuar</button>
+        <p className="loginsignup-login">Já possui conta?</p> <span>Login</span>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name='' id='' />
+          <p>Ao continuar, concordo com os termos de uso e políticas de privacidade.</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
